d96c607294671ab97d824f8a2ddab5248db28202
Luis Diego Hernández Mora <luisdiher22@gmail.com>
2025-06-18 09:04:48 -0600

    Upgrade to .NET 8

--- DetectorEstafaCR/Data/Migrations/20250615195039_InitialCreate.Designer.cs
+++ DetectorEstafaCR/Data/Migrations/20250615195039_InitialCreate.Designer.cs
@@ -18,7 +18,7 @@ namespace DetectorEstafaCR.Data.Migrations
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "7.0.20");
+            modelBuilder.HasAnnotation("ProductVersion", "8.0.17");
 
             modelBuilder.Entity("DetectorEstafaCR.Models.ReportedEntry", b =>
                 {
